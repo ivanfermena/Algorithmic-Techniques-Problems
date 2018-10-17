2481234240af18a3e19355dab91e1fff8e93eb41
ivanfermena <ivanfermena@gmail.com>
2018-10-17 23:44:29 +0200

    Ejercicio del juez 34 con solucion buena con conjuntos disjuntos

--- juez34/main.cpp
+++ juez34/main.cpp
@@ -7,68 +7,56 @@
 #include <vector>
 #include <string>
 
-#include "Grafo.h"
+#include "ConjuntosDisjuntos.h"
 
 using Map = std::vector<std::string>;
 
 // grafo implícito en el mapa
 class Manchas {
     public:
-        Manchas(Map const& M) : rows(M.size()), columns(M[0].size()), marked(rows, std::vector<bool>(columns, false)), _numero(0), _maximo(0) {
+        Manchas(Map const& M) : rows(M.size()), columns(M[0].size()), marked(rows, std::vector<bool>(columns, false)),_maximo(0), conj(rows*columns) {
             for(int i = 0; i < rows; ++i) {
                 for (int j = 0; j < columns; ++j) {
                     if (M[i][j] == '#' && !marked[i][j]) { // se recorre una nueva mancha
-                        ++_numero;
-                        int nuevotam = 0;
-                        dfs(M, i, j, nuevotam);
-                        _maximo = std::max(_maximo, nuevotam);
+                        dfs(M, i, j);
+                        _maximo = std::max(_maximo, std::abs(conj.tam(i * columns + j)));
                     }
                 }
             }
         }
 
-        void reordenar(Map const& M, int row, int column){
-            cpy_marked = marked;
-            int nuevotam = 0;
-            new_dfs(M, row, column, nuevotam);
-            _maximo = std::max(_maximo, nuevotam);
-            marked[row][column] = true;
-        }
-
         int maximo() const { return _maximo; }
 
+        void update(Map const& M, int i, int j){
+            marked[i][j] = true;
+            for (auto d : dirs) {
+                int ni = i + d.first, nj = j + d.second;
+                if (correcta(ni,nj) && M[ni][nj] == '#') {
+                    conj.unir(i * columns + j , ni * columns + nj);
+                }
+            }
+            _maximo = std::max(_maximo, std::abs(conj.tam(i * columns + j)));
+        }
+
     private:
-        int rows, columns; // tamaño del mapa
-        std::vector<std::vector<bool>> marked; // marked[i][j] = se ha visitado <i,j>?
-        std::vector<std::vector<bool>> cpy_marked;
-        int _numero; // número de manchas
-        int _maximo; // tamaño de la mancha más grande
+        int rows, columns;
+        std::vector<std::vector<bool>> marked;
+        int _maximo;
+        ConjuntosDisjuntos conj;
 
         bool correcta(int i, int j) const {
             return 0<= i && i<rows && 0<= j && j<columns;
         }
 
         const std::vector<std::pair<int,int>> dirs = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,-1},{-1,1},{1,-1}};
-        // recorrido en profundidad de la mancha del vértice <i,j>
 
-        void dfs(Map const& M, int i, int j, int & tam) {
+        void dfs(Map const& M, int i, int j) {
             marked[i][j] = true;
-            ++tam;
             for (auto d : dirs) {
                 int ni = i + d.first, nj = j + d.second;
                 if (correcta(ni,nj) && M[ni][nj] == '#' && !marked[ni][nj]) {
-                    dfs(M, ni, nj, tam);
-                }
-            }
-        }
-
-        void new_dfs(Map const& M, int i, int j, int & tam){
-            cpy_marked[i][j] = false;
-            ++tam;
-            for (auto d : dirs) {
-                int ni = i + d.first, nj = j + d.second;
-                if (correcta(ni,nj) && M[ni][nj] == '#' && cpy_marked[ni][nj]) {
-                    new_dfs(M, ni, nj, tam);
+                    conj.unir(i * columns + j , ni * columns + nj);
+                    dfs(M, ni, nj);
                 }
             }
         }
@@ -95,7 +83,7 @@ bool resuelveCaso() {
 
     std::cout << manchas.maximo() << " ";
 
-    // Insertamos tres elementos
+    // Insertamos elementos
 
     int num_news_elem, new_row, new_colum;
 
@@ -108,7 +96,8 @@ bool resuelveCaso() {
         aux[new_colum-1] = '#';
         map[new_row-1] = aux;
 
-        manchas.reordenar(map, new_row-1, new_colum-1);
+        manchas.update(map, new_row-1, new_colum-1);
+
         std::cout << manchas.maximo() << " ";
     }
     std::cout << "\n";
