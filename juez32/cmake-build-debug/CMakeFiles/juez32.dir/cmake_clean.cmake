be409bb739fa6ad983ecf9a2a5f77c9988be57e1
ivanfermena <ivanfermena@gmail.com>
2018-10-20 10:46:27 +0200

    Test gitignore and juez32

---
+++ juez32/cmake-build-debug/CMakeFiles/juez32.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/juez32.dir/main.cpp.o"
+  "juez32.pdb"
+  "juez32"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/juez32.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
